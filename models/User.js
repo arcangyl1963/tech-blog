540aca7182752c2bf07c6f816c545200bb2d7c04
Jim Harris <arcangyl@gmail.com>
2021-05-19 18:56:22 -0400

    Updated models for Comment, Post, and User

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+//create the user model
+class User extends Model {
+    // check the user's password at entry
+    checkPw(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    },
+    {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            beforeUpdate: async (updateUserData) => {
+                updatedUserData.password = await bcrypt.hash(updateUserData.password, 10);
+                return updateUserData;
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true;
+        underscored: true,
+        modelName: 'User'
+    }
+);
+module.exports = User;
