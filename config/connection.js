10896e8bbf03c82e1953e75e92e671e3f3d72bac
Jim Harris <arcangyl@gmail.com>
2021-05-23 22:11:23 -0400

    Changed connection to JAWSDB and updated the dotenv file to prepare for deployment to Heroku

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,8 @@ require('dotenv').config();
 
 const sequelize = process.env.JAWSDB_URL
     ? new Sequelize(process.env.JAWSDB_URL)
-    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
+    : new Sequelize(process.env.MYSQL_URL, {
+        dialect: 'mysql'
     });
 
     module.exports = sequelize;
